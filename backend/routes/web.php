2c51b11aecb2145963390a7dbc9db013abb06b60
Esdras França <esdras.franca.85@gmail.com>
2026-05-25 07:14:41 -0300

    feat: Inclusão da rora de list

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/api', [PokemonController::class, 'index']);
 Route::get('/api/pokemon', [PokemonController::class, 'getPokemon']);
 Route::get('/api/battle', [PokemonController::class, 'battle']);
+Route::get('/api/pokemons', [PokemonController::class, 'list']);
